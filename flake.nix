bcb398a15b4684c74cf1000ffc4c38f560439b2f
Eorclus <aldzikridp@gmail.com>
2023-09-14 20:52:07 +0700

    update .gitignore

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Proposal Pra-Skripsi";
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
+
+  };
+
+  outputs = { self, nixpkgs }: let
+      forAllSystems = nixpkgs.lib.genAttrs [ "x86_64-linux" ];
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              fcgi
+              llvmPackages_15.clang
+            ];
+          };
+        });
+    };
+}
